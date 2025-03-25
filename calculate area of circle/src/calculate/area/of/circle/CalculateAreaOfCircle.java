2a406360a92881c1af8784c1879cc1c0a83f010b
atiqur rahman <atiqurrahman.ar001@gmail.com>
2025-03-25 16:23:35 +0600

    25 marc

---
+++ calculate area of circle/src/calculate/area/of/circle/CalculateAreaOfCircle.java
@@ -0,0 +1,27 @@
+
+package calculate.area.of.circle;
+
+import java.util.Scanner;
+
+
+public class CalculateAreaOfCircle {
+
+   
+    public static void main(String[] args) {
+      
+        Scanner circle=new Scanner(System.in);
+        System.out.println("enter radious");
+        int numner=inew Scanner(System.in);
+        System.out.prnput.nextInt();
+        
+       float result= (float)3.1416 *(numner*numner);
+        System.out.println(result);
+//            int radious=circle.nextInt();
+        
+        
+// float area=(float) (Math.PI*Math.pow(radious,2));
+                
+//        System.out.println("area of circle is "+ area);
+    }
+    
+}
